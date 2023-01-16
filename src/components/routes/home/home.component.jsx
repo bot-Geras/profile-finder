cd09cf0c0faa16af75dd81b9a877ebf782cbb76c
Brian <briandave771@gmail.com>
2023-01-16 05:53:59 +0300

    wip

--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useState } from "react";
 
 import SearchBar from "../../searchBar/search.component";
 import ProfileCardList from "../../profile-card-list.component";
@@ -7,23 +7,29 @@ import { useGetUsersQuery } from "../../../features/dataSlices";
 function Home() {
   const { data } = useGetUsersQuery();
   const [searchField, setSearchField] = useState("");
-  const [userProfile, setUserProfile] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState(userProfile);
-  console.log(data);
+  // const [userProfile, setUserProfile] = useState([]);
+  // const [filteredUsers, setFilteredUsers] = useState(userProfile);
+  // console.log(data);
 
-  useEffect(() => {
-    setUserProfile(data);
-  }, [data]);
+  // useEffect(() => {
+  //   setUserProfile(data);
+  // }, [data]);
 
-  useEffect(() => {
-    const filterNewUsers = userProfile.filter((user) => {
-      return user.name.toLowerCase().includes(searchField);
-    });
-    setFilteredUsers(filterNewUsers);
-  }, [userProfile, searchField]);
+  // useEffect(() => {
+  //   const filterNewUsers = userProfile.filter((user) => {
+  //     return user.name.toLowerCase().includes(searchField);
+  //   });
+  //   setFilteredUsers(filterNewUsers);
+  // }, [userProfile, searchField]);
+  const keys = ["name", "email"]
+  function search(data) {
+    return data.filter((item) => 
+    keys.some(key => item[key].toLowerCase().includes(searchField))
+    )
+  }
 
   function onSearchHandler(e) {
-    const searchString = e.target.value.toLowerCase();
+    const searchString = e.target.value
     setSearchField(searchString);
   }
 
@@ -31,7 +37,7 @@ function Home() {
     <div className="h-screen bg-orange-100 flex flex-col items-center">
       <SearchBar search={onSearchHandler} />
 
-      <ProfileCardList list={filteredUsers} />
+      <ProfileCardList list={search(data)} />
     </div>
   );
 }
