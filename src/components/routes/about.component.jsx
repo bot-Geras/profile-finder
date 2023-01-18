498468a1b72780022e41b5bee7d8adb5e78cbd56
Brian <briandave771@gmail.com>
2023-01-18 09:36:54 +0300

    refactor style property

--- src/components/routes/about.component.jsx
+++ src/components/routes/about.component.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 const About = () => {
   return (
-    <div class="max-w-sm m-auto my-8 rounded flex flex-col border-grey-800 overflow-hidden shadow-lg">
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Profile Finder</div>
-        <p class="text-gray-700 text-base">
+    <div className="max-w-sm m-auto my-8 rounded flex flex-col border-grey-800 overflow-hidden shadow-lg">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Profile Finder</div>
+        <p className="text-gray-700 text-base">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
           quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
           nihil.
         </p>
       </div>
-      <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
           ReactJS
         </span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
           ReduxToolkit
         </span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
           React Router
         </span>
       </div>
